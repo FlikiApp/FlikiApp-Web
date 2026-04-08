0372e177ab4fe0d67ff5e7cb1d874b4b31d137b1
taenam <tkim356@wisc.edu>
2026-04-07 20:56:59 -0400

    Fix feature grid description to reference rankings

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -82,7 +82,7 @@ export const FEATURES: Feature[] = [
   {
     title: 'Custom Recommendations',
     description:
-      'Get tailored suggestions based by your unique ranking profile — not generic algorithms.',
+      'Get tailored suggestions based on your rankings — not generic algorithms.',
   },
   {
     title: 'Sharing',
