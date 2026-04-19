aec3e9eb4d002707362b9ba6d9ae3cb2c8616709
taenam <tkim356@wisc.edu>
2026-04-19 15:41:18 -0400

    Refactor HomePage hero phone to use Tilt component
    Replaces the inline useMotionValue + useSpring + onMouseMove wiring
    with the reusable Tilt wrapper. Same behavior, ~25 fewer lines in
    HomePage.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useRef } from 'react'
-import { motion, useMotionValue, useScroll, useSpring, useTransform } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import Seo from '../components/Seo'
 import StructuredData from '../components/StructuredData'
 import SectionWrapper from '../components/ui/SectionWrapper'
@@ -10,6 +10,7 @@ import FeatureCard from '../components/ui/FeatureCard'
 import DustMotes from '../components/ui/DustMotes'
 import PhoneMockup from '../components/ui/PhoneMockup'
 import SplitText from '../components/ui/SplitText'
+import Tilt from '../components/ui/Tilt'
 import TrendingToday from '../components/ui/TrendingToday'
 import homeScreenshot from '../components/screenshots/home.png'
 import { HOW_IT_WORKS_STEPS, FEATURES } from '../lib/constants'
@@ -24,14 +25,6 @@ export default function HomePage() {
   // parallax planes behind the scrolling content.
   const glowY = useTransform(scrollYProgress, [0, 1], ['0%', '40%'])
   const vignetteY = useTransform(scrollYProgress, [0, 1], ['0%', '15%'])
-
-  // Cursor-tracking 3D tilt for the hero phone mockup
-  const pointerX = useMotionValue(0)
-  const pointerY = useMotionValue(0)
-  const springCfg = { stiffness: 160, damping: 22, mass: 0.6 }
-  const tiltX = useSpring(useTransform(pointerY, [-0.5, 0.5], [10, -10]), springCfg)
-  const tiltY = useSpring(useTransform(pointerX, [-0.5, 0.5], [-14, 14]), springCfg)
-  const liftY = useSpring(useTransform(pointerY, [-0.5, 0.5], [-4, 4]), springCfg)
 
   return (
     <motion.div
@@ -117,28 +110,10 @@ export default function HomePage() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
               className="flex justify-center lg:justify-end"
-              style={{ perspective: 1200 }}
-              onMouseMove={(e) => {
-                const rect = e.currentTarget.getBoundingClientRect()
-                pointerX.set((e.clientX - rect.left) / rect.width - 0.5)
-                pointerY.set((e.clientY - rect.top) / rect.height - 0.5)
-              }}
-              onMouseLeave={() => {
-                pointerX.set(0)
-                pointerY.set(0)
-              }}
             >
-              <motion.div
-                style={{
-                  rotateX: tiltX,
-                  rotateY: tiltY,
-                  y: liftY,
-                  transformStyle: 'preserve-3d',
-                }}
-                className="will-change-transform"
-              >
+              <Tilt maxTilt={12} lift={4}>
                 <PhoneMockup screenshot={homeScreenshot} />
-              </motion.div>
+              </Tilt>
             </motion.div>
           </div>
         </div>
